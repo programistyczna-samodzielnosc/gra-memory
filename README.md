f60de155c44734dd84e7a9dab854be720337bf0c
Cyprian Gepfert <cgepfert@gmail.com>
2019-05-16 23:02:39 +0200

    Losowanie kolejności obrazków

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # gra-memory
+
+- gra dla 1 gracza
+- plansza z parami obrazkow(w sumie liczba parzysta)
+- 4x4 = 16 = 8x2 = 8 par
+- obrazki są wymieszane
+- celem jest znalezienie par w jak najmniejszej ilości ruchów
+- wynik to liczba ruchów
+- w 1 ruchu można odkryć 2 obrazki.
+- Jeżeli obrazki są różne to oba zakrywamy
+- Jezeli obrazki są te same to ściągamy z planszy.
---
+++ memory.js
@@ -0,0 +1,84 @@
+function Memory() {
+    const NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7];
+    const IMAGES = [
+        {
+            name: 'chmurka',
+            src: 'chmurka.png'
+        },
+        {
+            name: 'gwiazdka',
+            src: 'gwiazdka.png'
+        },
+        {
+            name: 'romb',
+            src: 'romb.png'
+        },
+        {
+            name: 'serduszko',
+            src: 'serduszko.png'
+        },
+        {
+            name: 'strzalka_dol',
+            src: 'strzalka_dol.png'
+        },
+        {
+            name: 'strzalka_gora',
+            src: 'strzalka_gora.png'
+        },
+        {
+            name: 'strzalka_lewo',
+            src: 'strzalka_lewo.png'
+        },
+        {
+            name: 'strzalka_prawo',
+            src: 'strzalka_prawo.png'
+        }
+    ];
+
+    const FINAL_NUMBERS = shuffle(NUMBERS);
+    console.log('blblele', FINAL_NUMBERS);
+
+    let board = document.querySelector('.board')
+
+    displayBoard();
+
+    function displayBoard() {
+        let html = FINAL_NUMBERS.map((number)=>{
+            return htmlImage(IMAGES[number])
+        }).join('');
+        board.innerHTML = html;
+    }
+
+    function htmlImage(image) {
+        return `
+            <div class="image image--${image.name}">
+                <img src="memory/${image.src}">
+            </div>
+        `
+    }
+
+
+    function shuffle(numbers) {
+        let copy = numbers.concat();
+        let result = [];
+
+        for (let i = 0; i < numbers.length; i++) {
+            let randomNumber = getRandomInt(0, copy.length);
+            result.push(copy[randomNumber]);
+            copy = [
+                ...copy.slice(0, randomNumber),
+                ...copy.slice(randomNumber + 1, copy.length)
+            ]
+        }
+        return result;
+    }
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+}
+
+
+window.onload = Memory;
